56eb7604524c3a2c6672685e245a34e39c20a760
Igor Silveira <igor-silveira10@hotmail.com>
2020-05-15 12:49:57 -0300

    Feature:(MacaddressValidation.js, TaskRoutes.js) Added MacaddressValidation

---
+++ src/middlewares/MacaddressValidation.js
@@ -0,0 +1,11 @@
+const MacaddressValidation = (req, res, next) => {
+  if (!req.body.macaddress) {
+    return res.status(400).json([{
+      error: 'Macaddress é obrigatório!',
+    },
+    ]);
+  }
+  return next();
+};
+
+module.exports = MacaddressValidation;
--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -4,11 +4,12 @@ const router = express.Router();
 
 const TaskController = require('../controller/TaskController');
 const TaskValidation = require('../middlewares/TaskValidation');
+const MacaddressValidation = require('../middlewares/MacaddressValidation');
 
 router.post('/task', TaskValidation, TaskController.create);
 
 router.put('/task/:id', TaskValidation, TaskController.update);
 
-router.get('/task', TaskController.all);
+router.get('/task', MacaddressValidation, TaskController.all);
 
 module.exports = router;
